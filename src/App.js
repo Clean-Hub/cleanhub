38ae372e66ca1ac9c4392f34833799f4766be46a
phantware <phantmoney2011@gmail.com>
2022-11-05 12:42:01 +0100

    google font adjusted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/navbar/Navbar'
+import HomeHero from './components/homeHero/HomeHero'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <HomeHero />
     </div>
   )
 }
