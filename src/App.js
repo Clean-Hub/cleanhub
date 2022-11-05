93978e1e3fe7bcd25d7eec7783a044d6a51117db
phantware <phantmoney2011@gmail.com>
2022-11-05 12:00:39 +0100

    navbar logo added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './components/navbar/Navbar'
+
 function App() {
   return (
     <div className='App'>
-      <h1>Welcome to Clean Hub</h1>
+      <Navbar />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import './navbar.css'
+import logo from '../../assets/img/logo.png'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='navbarContainer'>
+      <div className='navbarLeft'>
+        <img src={logo} alt='logo' />
+      </div>
+      <div className='navbarRight'>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>HOW IT WORKS</li>
+          <li>CONTACT</li>
+          <li>BLOG</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
