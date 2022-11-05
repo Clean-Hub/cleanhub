835037b2f04d2d0a24c1916e5436837ead3f1022
phantware <phantmoney2011@gmail.com>
2022-11-05 12:09:36 +0100

    navbar component styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = () => {
   return (
     <div className='navbarContainer'>
       <div className='navbarLeft'>
-        <img src={logo} alt='logo' />
+        <img src={logo} alt='logo' className='navbarLogo' />
       </div>
       <div className='navbarRight'>
-        <ul>
-          <li>HOME</li>
-          <li>ABOUT US</li>
-          <li>HOW IT WORKS</li>
-          <li>CONTACT</li>
-          <li>BLOG</li>
+        <ul className='navbarUl'>
+          <li className='navbarLi'>HOME</li>
+          <li className='navbarLi'>ABOUT US</li>
+          <li className='navbarLi'>HOW IT WORKS</li>
+          <li className='navbarLi'>CONTACT</li>
+          <li className='navbarLi'>BLOG</li>
         </ul>
       </div>
     </div>
