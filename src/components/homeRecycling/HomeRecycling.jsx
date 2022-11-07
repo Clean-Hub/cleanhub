e6b339ca5046fdcd01490c8c9ad969aa8f6ea072
phantware <phantmoney2011@gmail.com>
2022-11-07 22:04:24 +0100

    homeRecycling component styled

--- src/components/homeRecycling/HomeRecycling.jsx
+++ src/components/homeRecycling/HomeRecycling.jsx
@@ -4,6 +4,12 @@ import './homeRecycling.css'
 const HomeRecycling = () => {
   return (
     <div className='homeRecyclingContainer'>
+      <div className='homeRecyclingInfo'>
+        <h2 className='homeRecyclingInfoH2'>HOW IT WORKS</h2>
+        <p className='homeRecyclingInfoP'>
+          Make some cash from recycling that trash - Learn how it works
+        </p>
+      </div>
       <RecyclingLogo />
     </div>
   )
