72287e39ba3590bae0d0ac82eb95ac68240d7a4e
phantware <phantmoney2011@gmail.com>
2022-11-07 23:40:24 +0100

    fontawesome packages installed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className='footerContainer'>
+      <div className='footerT'>
+        <div className='footerTL'></div>
+        <div className='footerTR'></div>
+      </div>
+      <div className='footerB'>
+        <div className='footerBConnect'>
+          <span></span>
+          <h3>CONNECT WITH US</h3>
+        </div>
+        <div className='footerBSocial'>
+          <div className='footerBSocialL'>
+            <ul>
+              <li>Terms</li>
+              <li>Privacy</li>
+            </ul>
+          </div>
+          <div className='footerBSocialR'>
+            <ul>
+              <li>Terms</li>
+              <li>Privacy</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
