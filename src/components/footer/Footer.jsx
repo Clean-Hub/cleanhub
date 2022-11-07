a571d82db9f67cf9862b763c869d8b523cefb124
phantware <phantmoney2011@gmail.com>
2022-11-07 23:59:23 +0100

    footer ul created

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
-
+import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+import logo from '../../assets/img/logo.png'
 const Footer = () => {
   return (
     <div className='footerContainer'>
       <div className='footerT'>
-        <div className='footerTL'></div>
-        <div className='footerTR'></div>
+        <div className='footerTL'>
+          <img src={logo} alt='logo' className='footerLogo' />
+
+          <p className='footerText'>SUBSCRIBE TO OUR NEWSLETTER</p>
+          <div className='footerSubscribe'>
+            <input type='text' placeholder='Enter your email address' />
+            <button className='footerSubscribeBtn'>SUBSCRIBE</button>
+          </div>
+        </div>
+        <div className='footerTR'>
+          <div className=''>
+            <h4 className=''>ABOUT US</h4>
+            <ul className=''>
+              <li className=''>Who we are</li>
+              <li className=''>Our Mission</li>
+              <li className=''>Services</li>
+              <li className=''>Our Projects</li>
+              <li className=''>Partnerships</li>
+            </ul>
+          </div>
+          <div className=''>
+            <h4 className=''>RESOURCES</h4>
+            <ul className=''>
+              <li className=''>Careers</li>
+              <li className=''>Blog</li>
+              <li className=''>My Points</li>
+              <li className=''>Gallery</li>
+              <li className=''>FAQS</li>
+            </ul>
+          </div>
+          <div className=''>
+            <h4 className=''>CONTACT US</h4>
+          </div>
+        </div>
       </div>
       <div className='footerB'>
         <div className='footerBConnect'>
@@ -21,8 +60,18 @@ const Footer = () => {
           </div>
           <div className='footerBSocialR'>
             <ul>
-              <li>Terms</li>
-              <li>Privacy</li>
+              <li>
+                <FontAwesomeIcon icon={faFacebook} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faTwitter} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faInstagram} />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faYoutube} />
+              </li>
             </ul>
           </div>
         </div>
