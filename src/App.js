39b438a899c01f6a67178f898d26ac4a907afb30
phantware <phantmoney2011@gmail.com>
2022-11-07 23:31:40 +0100

    footer component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/navbar/Navbar'
 import './app.css'
 import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className='appContainer'>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
