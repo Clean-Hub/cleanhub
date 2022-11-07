d3e635d0d0003ce5f7604e7617da2cfa3a3ace41
phantware <phantmoney2011@gmail.com>
2022-11-07 22:09:40 +0100

    cardInfo added to demoData

---
+++ src/components/homeRecyclingCard/HomeRecyclingCard.jsx
@@ -0,0 +1,7 @@
+import './homeRecyclingCard.css'
+
+const HomeRecyclingCard = () => {
+  return <div className='homeRecyclingCardContainer'>HomeRecyclingCard</div>
+}
+
+export default HomeRecyclingCard
