b3660016f8db6b899962c2f1a8f44a425e15fdbf
phantware <phantmoney2011@gmail.com>
2022-11-07 18:31:03 +0100

    homeIntro component populated

--- src/components/homeIntro/HomeIntro.jsx
+++ src/components/homeIntro/HomeIntro.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import './homeIntro.css'
+import circle from '../../assets/img/cycle.png'
 
 const HomeIntro = () => {
   return (
-    <div>
-      HomeIntro HomeIntroHomeIntroHomeIntroHomeIntroHomeIntroHomeIntroHomeIntro
-      HomeIntro HomeIntro HomeIntro HomeIntro HomeIntro HomeIntro HomeIntro
-      HomeIntro HomeIntro
+    <div className='homeIntroContainer'>
+      <div className='homeIntroL'>
+        <img src={circle} alt='homeIntroLImg' />
+      </div>
+      <div className='homeIntroR'>
+        <div className='homeIntroH2'>WHO WE ARE</div>
+        <div className='homeIntroH1'>
+          Yes, we believe in a cleaner and healthy LAGOS!
+        </div>
+        <div className='homeIntroP'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et mauris
+          libero suspendisse in urna, fusce imperdiet aliquet. Tellus, mollis
+          commodo turpis netus ultrices ut. Senectus in a vestibulum aliquet
+          amet ornare tellus egestas. Condimentum sodales a faucibus eu enim.
+        </div>
+        <button className='homeIntroBtn'>LEARN MORE</button>
+      </div>
     </div>
   )
 }
