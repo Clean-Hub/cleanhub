642488e55eea71050aeaff0b0124c5bdb528dec4
phantware <phantmoney2011@gmail.com>
2022-11-14 22:20:16 +0100

    remmemberMe toggle styled

--- src/components/rememberMe/RememberMe.jsx
+++ src/components/rememberMe/RememberMe.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import './rememberMe.css'
 
 const RememberMe = ({ onChange }) => {
-  return <input type='checkbox' onChange={onChange} />
+  return (
+    <label className='inputWrapper'>
+      <input type='checkbox' onChange={onChange} className='input' />
+      <span className='inputSpan' />
+    </label>
+  )
 }
 
 export default RememberMe
