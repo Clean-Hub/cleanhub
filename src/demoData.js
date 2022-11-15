f1f58727fd9845a079fbc1b002f750979ef194ab
phantware <phantmoney2011@gmail.com>
2022-11-15 17:34:15 +0100

    demoData populated

--- src/demoData.js
+++ src/demoData.js
@@ -2,6 +2,11 @@ import dropOff from './assets/img/drop-off.png'
 import cashRedemption from './assets/img/cash-redemption.png'
 import earnPoints from './assets/img/earn-points.png'
 import pickUp from './assets/img/pick-up.png'
+import waste1 from './assets/img/waste1.png'
+import waste2 from './assets/img/waste2.jpg'
+import waste3 from './assets/img/waste3.jfif'
+import waste4 from './assets/img/waste4.jpg'
+import waste5 from './assets/img/waste5.jpg'
 
 const logo = [
   {
@@ -21,6 +26,30 @@ const logo = [
     name: 'CASH REDEMPTION',
   },
 ]
+
+export const sliderImg = [
+  {
+    id: 1,
+    photo: waste1,
+  },
+  {
+    id: 2,
+    photo: waste2,
+  },
+  {
+    id: 3,
+    photo: waste3,
+  },
+  {
+    id: 4,
+    photo: waste4,
+  },
+  {
+    id: 5,
+    photo: waste5,
+  },
+]
+
 export const cardInfo = [
   `Relax and have us come do all the work. Schedule a pickup today and we'd be glad to head right up`,
   ` Find a dropoff center around you and deliver your recyclable waste locally`,
