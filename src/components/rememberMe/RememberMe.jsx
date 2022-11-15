6e9cbbc9873d64b96580132a171646361b2ab2d7
phantware <phantmoney2011@gmail.com>
2022-11-15 14:35:46 +0100

    rememberme feature adjusted"

--- src/components/rememberMe/RememberMe.jsx
+++ src/components/rememberMe/RememberMe.jsx
@@ -1,11 +1,27 @@
+// import { useState } from 'react'
 import './rememberMe.css'
 
 const RememberMe = ({ onChange }) => {
+  // const [click, setClick] = useState(false)
+
+  // const toggleHandler = () => {
+  //   setClick(!click)
+  // }
   return (
+    // This is another pattern of button slider
     <label className='inputWrapper'>
       <input type='checkbox' onChange={onChange} className='input' />
       <span className='inputSpan' />
     </label>
+
+    // This is another pattern of button slider
+    // <div className='toggle-btn'>
+    //   <div
+    //     onClick={toggleHandler}
+    //     className={click ? 'toggle-btn--circle toggle' : 'toggle-btn--circle'}
+    //   ></div>
+    //   <div className='toggle-btn--slider'></div>
+    // </div>
   )
 }
 
