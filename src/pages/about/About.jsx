8531d27d3ed7fbc7820bfda656eb9030ed5ea269
phantware <phantmoney2011@gmail.com>
2022-11-16 12:21:03 +0100

    aboutIntro component created

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,12 @@
 import AboutHero from '../../components/aboutHero/AboutHero'
+import AboutIntro from '../../components/aboutIntro/AboutIntro'
 import './about.css'
 
 const About = () => {
   return (
     <div className='aboutContainer'>
       <AboutHero />
+      <AboutIntro />
     </div>
   )
 }
